2e46c8990dba222982d0ae3577f37d505009de69
Samandar <69756664+samandareo@users.noreply.github.com>
2022-10-28 08:54:42 +0500

    update

--- prAuto.py
+++ prAuto.py
@@ -1,8 +1,8 @@
 from pyrogram import Client
 
 from pyrogram import filters
-api_id =1074334
-api_hash ="cc60986b51f899ed64095de24279723f"
+api_id = 123456 #api_id yoziladi 
+api_hash ="15551d5sd5s15ds5d15sd5sd18" # o'z api hash adresizzi yozasiz
 
 APP = Client("tilonlike", api_id, api_hash)
 @APP.on_message(filters.text)
